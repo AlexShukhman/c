2d918c1d6f158c6cef0366142d819aad8a7b26ad
alexshukhman <alex.jacob.shukhman@gmail.com>
2022-02-13 17:16:21 -0500

    Silly mistake

--- scripts/stranger.js
+++ scripts/stranger.js
@@ -12,37 +12,7 @@
     You give me the pleasure of your eyes, face, flesh, as we pass, you take of my beard, breast, hands, in return,
     I am not to speak to you, I am to think of you when I sit alone or wake at night alone,
     I am to wait, I do not doubt I am to meet you again,
-    I am to see to it that I do not lose you.
-    There it was I saw what I shall never forget
-    And never retrieve.
-    Monstrous and beautiful to human eyes, hard to
-    believe,
-    He lay, yet there he lay,
-    Asleep on the moss, his head on his polished cleft
-    small ebony hoves,
-    The child of the doe, the dappled child of the deer.
-    
-    Surely his mother had never said, "Lie here
-    Till I return," so spotty and plain to see
-    On the green moss lay he.
-    His eyes had opened; he considered me.
-    
-    I would have given more than I care to say
-    To thrifty ears, might I have had him for my friend
-    One moment only of that forest day:
-    
-    Might I have had the acceptance, not the love
-    Of those clear eyes;
-    Might I have been for him in the bough above
-    Or the root beneath his forest bed,
-    A part of the forest, seen without surprise.
-    
-    Was it alarm, or was it the wind of my fear lest he
-    depart
-    That jerked him to his jointy knees,
-    And sent him crashing off, leaping and stumbling
-    On his new legs, between the stems of the white
-    trees?`;
+    I am to see to it that I do not lose you.`;
 
     const id = 'stranger';
 
